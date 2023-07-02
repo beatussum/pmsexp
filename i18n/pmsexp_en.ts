04600f492c446e49c2917eefdac1b15b52e247d1
Mattéo Rossillol‑‑Laruelle <beatussum@protonmail.com>
2023-07-02 16:46:45 +0200

    i18n: add english
    Signed-off-by: Mattéo Rossillol‑‑Laruelle <beatussum@protonmail.com>

---
+++ i18n/pmsexp_en.ts
@@ -0,0 +1,188 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en" sourcelanguage="en">
+<context>
+    <name>ButtonSelecterWidget</name>
+    <message>
+        <location filename="../include/gui/widgets/ButtonSelecterWidget.ui" line="22"/>
+        <location filename="../build/pmsexp_autogen/include/ui_ButtonSelecterWidget.h" line="75"/>
+        <source>Previous</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../include/gui/widgets/ButtonSelecterWidget.ui" line="29"/>
+        <location filename="../build/pmsexp_autogen/include/ui_ButtonSelecterWidget.h" line="77"/>
+        <source>Ctrl+Left</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../include/gui/widgets/ButtonSelecterWidget.ui" line="36"/>
+        <location filename="../build/pmsexp_autogen/include/ui_ButtonSelecterWidget.h" line="80"/>
+        <source>Ctrl+Right</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>Calibration</name>
+    <message>
+        <location filename="../include/gui/widgets/pages/Calibration.ui" line="27"/>
+        <location filename="../build/pmsexp_autogen/include/ui_Calibration.h" line="77"/>
+        <source>Real measure</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../include/gui/widgets/pages/Calibration.ui" line="33"/>
+        <location filename="../build/pmsexp_autogen/include/ui_Calibration.h" line="78"/>
+        <source>Enter the corresponding actual measurement (in cm).</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>ContourSelection</name>
+    <message>
+        <location filename="../include/gui/widgets/pages/ContourSelection.ui" line="22"/>
+        <location filename="../build/pmsexp_autogen/include/ui_ContourSelection.h" line="82"/>
+        <source>Previous</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../include/gui/widgets/pages/ContourSelection.ui" line="32"/>
+        <location filename="../build/pmsexp_autogen/include/ui_ContourSelection.h" line="83"/>
+        <source>Next</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../include/gui/MainWindow.ui" line="44"/>
+        <location filename="../build/pmsexp_autogen/include/ui_MainWindow.h" line="81"/>
+        <source>Quit</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../include/gui/MainWindow.ui" line="47"/>
+        <location filename="../build/pmsexp_autogen/include/ui_MainWindow.h" line="83"/>
+        <source>Quit the application.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../include/gui/MainWindow.ui" line="50"/>
+        <location filename="../build/pmsexp_autogen/include/ui_MainWindow.h" line="86"/>
+        <source>Ctrl+Q</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../include/gui/MainWindow.ui" line="59"/>
+        <location filename="../build/pmsexp_autogen/include/ui_MainWindow.h" line="88"/>
+        <source>Reset</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../include/gui/MainWindow.ui" line="62"/>
+        <location filename="../build/pmsexp_autogen/include/ui_MainWindow.h" line="90"/>
+        <source>Reset loaded files.</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>Upload</name>
+    <message>
+        <location filename="../include/gui/widgets/pages/Upload.ui" line="17"/>
+        <location filename="../build/pmsexp_autogen/include/ui_Upload.h" line="82"/>
+        <source>Data loading</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../include/gui/widgets/pages/Upload.ui" line="26"/>
+        <location filename="../build/pmsexp_autogen/include/ui_Upload.h" line="83"/>
+        <source>Load data</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../include/gui/widgets/pages/Upload.ui" line="43"/>
+        <location filename="../build/pmsexp_autogen/include/ui_Upload.h" line="84"/>
+        <source>Current status</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../include/gui/widgets/pages/Upload.ui" line="49"/>
+        <location filename="../build/pmsexp_autogen/include/ui_Upload.h" line="85"/>
+        <source>Loading status of data</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>gui::MainWindow</name>
+    <message>
+        <location filename="../src/gui/MainWindow.cpp" line="76"/>
+        <source>Data loading is complete.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/MainWindow.cpp" line="198"/>
+        <source>No contours found.</source>
+        <translation></translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../src/gui/MainWindow.cpp" line="213"/>
+        <source>%n contour(s) found.</source>
+        <translation>
+            <numerusform>%n contour found.</numerusform>
+            <numerusform>%n contours found.</numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="../src/gui/MainWindow.cpp" line="237"/>
+        <source>No frame can be retrieved.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/MainWindow.cpp" line="242"/>
+        <source>Error opening the file &quot;%1&quot;.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/MainWindow.cpp" line="293"/>
+        <source>Results already loaded!</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>gui::widgets::ButtonSelecterWidget</name>
+    <message>
+        <location filename="../src/gui/widgets/ButtonSelecterWidget.cpp" line="94"/>
+        <source>Run</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/widgets/ButtonSelecterWidget.cpp" line="94"/>
+        <source>Next</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>gui::widgets::pages::Calibration</name>
+    <message>
+        <location filename="../src/gui/widgets/pages/Calibration.cpp" line="32"/>
+        <source>Use the mouse to select the area to be scaled. Press &quot;%1&quot; to validate the selection and &quot;%2&quot; to delete the previous one.</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>gui::widgets::pages::ContourSelection</name>
+    <message>
+        <location filename="../src/gui/widgets/pages/ContourSelection.cpp" line="89"/>
+        <source>Contour area: %1 px.</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>gui::widgets::pages::Selection</name>
+    <message>
+        <location filename="../src/gui/widgets/pages/Selection.cpp" line="30"/>
+        <source>Use the mouse to select the area to be monitored. Press &quot;%1&quot; to validate the selection and &quot;%2&quot; to delete the previous one.</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
